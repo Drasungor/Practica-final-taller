c61eca2362f496255a726254c8e6eecb03e3dfef
Agustin <aguscambiano@gmail.com>
2020-09-04 00:25:52 -0300

    Hago ejercicio de template

---
+++ Agustin/6_8_2019/ej_5/cmake-build-debug/CMakeFiles/ej_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ej_5.dir/main.cpp.o"
+  "ej_5"
+  "ej_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ej_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

ab02eeb763d294b0e2b71244f60cc5e0040b0d36
Agustin <aguscambiano@gmail.com>
2020-09-09 18:26:39 -0300

    Arreglo error de ej de ordenar archivo

--- Agustin/2019/12_2_2019/ej_9_server/main.cpp
+++ Agustin/2019/12_2_2019/ej_9_server/main.cpp
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <string>
 
+#define SUMAR(x,y) x*y
+
 int get_server_fd(char* port) {
     int socket_fd = -1;
     struct addrinfo hints;
@@ -75,6 +77,7 @@ bool execute_program(int client_socket) {
 }
 
 int main() {
+    printf("%d\n", SUMAR(1+2, 3));
     int server_socket = get_server_fd("8080");
     if (server_socket == -1) {
         printf("Error de socket\n");
